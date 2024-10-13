c060e23d1facefe3a977e1339fbd26d4ec65bc73
patriciadinis11 <patricia65dinis@gmail.com>
2024-10-13 18:49:03 -0300

    aula 5

---
+++ script.js
@@ -0,0 +1,8 @@
+const songName = document.getElementById("song-name");
+
+const songs = document.getElementById("audio");
+
+const playButton = document.getElementById("play");
+
+songName.innerText = "All i ask";
+// songs.play();
